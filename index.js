b3bdc46e99ccd6bdff37a8ffb0d6fdf863e4ca1a
GiorgiTarsaidze <giorgi.tarsaidze.1@btu.edu.ge>
2023-09-12 14:20:10 +0400

    changed text colors in dark theme, added cross image in each task

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 let image=document.querySelector(".img");
 let background=document.querySelector(".background");
-let darkElement=document.querySelectorAll(".dark")
+let darkElement=document.querySelectorAll(".dark");
+let mainText=document.querySelectorAll(".main-text");
 
 let num=0;
 
 image.addEventListener("click",()=>{
     if(num==0){
-
-    
     document.body.style.backgroundColor="#171823"
     background.style.backgroundImage="url('/images/bg-desktop-dark.jpg')"
     image.src="./images/icon-sun.svg"
 
     for(let i=0;i<darkElement.length;i++){
         darkElement[i].classList.add("darkElement");
+    }
+
+    for(let z=0;z<mainText.length;z++){
+        mainText[z].style.color="#C8CBE7"
     }
     num=1
     }else{
@@ -23,6 +26,10 @@ image.addEventListener("click",()=>{
 
     for(let i=0;i<darkElement.length;i++){
         darkElement[i].classList.remove("darkElement");
+    }
+
+    for(let z=0;z<mainText.length;z++){
+        mainText[z].style.color="#494C6B"
     }
     num=0
     }
