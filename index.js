d351cbdb237c91f46261da5b2d3d772d9ebb95c1
GiorgiTarsaidze <giorgi.tarsaidze.1@btu.edu.ge>
2023-09-12 14:01:12 +0400

    implemented switcher from dark to light mode and vice versa

--- index.js
+++ index.js
@@ -2,13 +2,28 @@ let image=document.querySelector(".img");
 let background=document.querySelector(".background");
 let darkElement=document.querySelectorAll(".dark")
 
+let num=0;
 
 image.addEventListener("click",()=>{
+    if(num==0){
+
+    
     document.body.style.backgroundColor="#171823"
     background.style.backgroundImage="url('/images/bg-desktop-dark.jpg')"
     image.src="./images/icon-sun.svg"
 
     for(let i=0;i<darkElement.length;i++){
         darkElement[i].classList.add("darkElement");
+    }
+    num=1
+    }else{
+    document.body.style.backgroundColor="rgba(250,250,250,1)"
+    background.style.backgroundImage="url('/images/bg-desktop-light.jpg')"
+    image.src="./images/icon-moon.svg"
+
+    for(let i=0;i<darkElement.length;i++){
+        darkElement[i].classList.remove("darkElement");
+    }
+    num=0
     }
 })
